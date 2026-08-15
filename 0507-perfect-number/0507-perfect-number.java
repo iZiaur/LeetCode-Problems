4cb7e98e8b238c6ec4822dbaaf2e056e765230d0
Md Ziaur Rahman <iziaurofficial@gmail.com>
2026-08-16 02:26:33 +0530

    Time: 1895 ms (62.96%), Space: 42.3 MB (26.28%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if(num==1){
+            return false;
+       }
+        int sum=1;
+        for(int i=2;i<num;i++){
+            if(sum>num){
+                return false;
+            }
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+
+        if(sum==num){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
