36b461eb4a50b86b70944814fa35c5f538471f8d
Md Ziaur Rahman <iziaurofficial@gmail.com>
2026-09-10 18:23:55 +0530

    Time: 2 ms (98.84%), Space: 48.1 MB (76.68%) - LeetHub

---
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode create(ArrayList<Integer>arr,int start,int end){
+        if(start>end){
+            return null;
+        }
+        int mid=(start+end)/2;
+        TreeNode root=new TreeNode(arr.get(mid));
+        root.left=create(arr,start,mid-1);
+        root.right=create(arr,mid+1,end);
+
+        return root;
+    }
+    public void inorder(TreeNode root,ArrayList<Integer> arr){
+        if(root==null){
+            return;
+        }
+        inorder(root.left,arr);
+        arr.add(root.val);
+        inorder(root.right,arr);
+    }
+    public TreeNode balanceBST(TreeNode root) {
+        if(root==null){
+            return null;
+        }
+        ArrayList<Integer> arr=new ArrayList<>();
+        inorder(root,arr);
+        return create(arr,0,arr.size()-1);
+    }
+}
