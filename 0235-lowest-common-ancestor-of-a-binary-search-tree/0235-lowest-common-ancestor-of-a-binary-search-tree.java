c2c7acea04c8bc63b7281f45ae703c286504310f
Md Ziaur Rahman <iziaurofficial@gmail.com>
2026-09-12 18:21:29 +0530

    Time: 6 ms (96.93%), Space: 47.8 MB (16.82%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,39 +9,19 @@
  */
 
 class Solution {
-    public boolean lca(TreeNode root,TreeNode p,ArrayList<TreeNode>arr){
-        if(root==null){
-            return false;
-        }
-        arr.add(root);
-        if(root==p){
-            return true;
-        }
-
-        boolean left=lca(root.left,p,arr);
-        boolean right=lca(root.right,p,arr);
-        if(left||right){
-            return true;
-        }
-
-        arr.remove(arr.size()-1);
-        return false;
-    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null){
-            return null;
-        }
-        ArrayList<TreeNode> arr1=new ArrayList<>();
-        ArrayList<TreeNode> arr2=new ArrayList<>();
-        lca(root,p,arr1);
-        lca(root,q,arr2);
-        int i=0;
-        for(;i<Math.min(arr1.size(),arr2.size());i++){
-            if(arr1.get(i)!=arr2.get(i)){
-                break;
+        TreeNode curr=root;
+        while(curr!=null){
+            if(q.val<curr.val && p.val<curr.val){
+                curr=curr.left;
             }
-        }
+            else if(q.val>curr.val && p.val>curr.val){
+                curr=curr.right;
+            }else{
+                return curr;
+            }
 
-        return arr1.get(i-1);
+        }
+        return null;
     }
 }
